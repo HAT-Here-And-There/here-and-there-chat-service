b091ccf7065e8e763657142d2df9b7ca59777fba
10000001a <pgdn06288888@gmail.com>
2024-05-22 02:45:18 +0900

    fix: allowed origin에 와일드카드 사용을 위해 메서드 수정

--- src/main/java/com/hat/hereandthere/chatservice/config/WebSocketConfig.java
+++ src/main/java/com/hat/hereandthere/chatservice/config/WebSocketConfig.java
@@ -19,7 +19,7 @@ public class WebSocketConfig implements WebSocketConfigurer {
   public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
 
     registry.addHandler(placeChatHandler(), "/place/*")
-        .setAllowedOrigins("*")
+        .setAllowedOriginPatterns("*")
         .withSockJS();
   }
 
