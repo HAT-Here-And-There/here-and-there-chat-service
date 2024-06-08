abd1b379115f8dc99a07b681a33584324dfec5a5
Youngtae Kim <47503412+10000001a@users.noreply.github.com>
2024-06-08 18:29:22 +0900

    채팅 데이터 중복 저장을 막기 위해 저장 호출 위치 변경 (#16)
    * fix: 채팅 데이터 중복 저장을 막기 위해 저장 호출 위치 변경 (HAT-112)
    
    * refac: redis pub/sub 네이밍 개선

--- src/main/java/com/hat/hereandthere/chatservice/handler/RedisSubscriber.java
+++ src/main/java/com/hat/hereandthere/chatservice/handler/RedisSubscriber.java
@@ -1,11 +1,7 @@
 package com.hat.hereandthere.chatservice.handler;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.hat.hereandthere.chatservice.chat.ChatService;
 import com.hat.hereandthere.chatservice.chat.dto.ChatReceiveMessage;
-import com.hat.hereandthere.chatservice.chat.dto.ChatSendMessage;
-import com.hat.hereandthere.chatservice.chat.entity.Chat;
-import com.hat.hereandthere.chatservice.chat.entity.Reply;
 import com.hat.hereandthere.chatservice.utils.WebSocketSessionUtils;
 import java.io.IOException;
 import java.nio.charset.StandardCharsets;
@@ -23,68 +19,27 @@ import org.springframework.web.socket.WebSocketSession;
 
 @Slf4j
 @Component
-public class RedisMessageSubscriber extends MessageListenerAdapter {
-
-  private final ChatService chatService;
+public class RedisSubscriber extends MessageListenerAdapter {
 
   private final ConcurrentHashMap<Long, Set<WebSocketSession>> sessions = new ConcurrentHashMap<>();
-
-  // 세션 저장용
-
   private final ObjectMapper objectMapper;
 
 
-  public RedisMessageSubscriber(
-      ChatService chatService,
+  public RedisSubscriber(
       ObjectMapper objectMapper
   ) {
-    this.chatService = chatService;
     this.objectMapper = objectMapper;
   }
 
 
   @Override
   public void onMessage(@NonNull Message message, byte[] pattern) {
-    final ChatSendMessage chatSendMessage;
-    final ChatReceiveMessage chatReceiveMessage;
-
     try {
-      chatSendMessage = objectMapper.readValue(message.getBody(), ChatSendMessage.class);
-      if (chatSendMessage.originChatId() == null) {
-        final Chat chat = chatService.saveChat(
-            chatSendMessage.userId(),
-            getPlaceId(message),
-            chatSendMessage.content()
-        );
-
-        chatReceiveMessage = new ChatReceiveMessage(
-            chat.getId(),
-            chat.getUserId(),
-            chat.getPlaceId(),
-            chat.getContent(),
-            null,
-            chat.getTimestamp()
-        );
-      } else {
-        final Reply reply = chatService.saveReply(
-            chatSendMessage.userId(),
-            chatSendMessage.content(),
-            chatSendMessage.originChatId()
-        );
-
-        chatReceiveMessage = new ChatReceiveMessage(
-            reply.getId(),
-            reply.getUserId(),
-            getPlaceId(message),
-            reply.getContent(),
-            chatSendMessage.originChatId(),
-            reply.getTimestamp()
-        );
-      }
-
-      sendMessage(chatReceiveMessage, getActiveSessionSet(message));
-    } catch (IOException e) {
-      throw new RuntimeException(e);
+      final ChatReceiveMessage chatReceiveMessage = objectMapper.readValue(message.getBody(),
+          ChatReceiveMessage.class);
+      sendToSessions(chatReceiveMessage, getActiveSessionSet(message));
+    } catch (Exception e) {
+      log.error("Exception on parsing. {}", e.getMessage());
     }
   }
 
@@ -110,15 +65,6 @@ public class RedisMessageSubscriber extends MessageListenerAdapter {
     });
   }
 
-  public boolean checkSession(WebSocketSession session) {
-    final Long placeId = WebSocketSessionUtils.getChannelId(session);
-    if (sessions.containsKey(placeId)) {
-      return sessions.get(placeId).contains(session);
-    }
-
-    return false;
-  }
-
   private Long getPlaceId(@NonNull Message message) {
     return Long.parseLong(new String(message.getChannel(), StandardCharsets.UTF_8).split(":")[1]);
   }
@@ -127,7 +73,7 @@ public class RedisMessageSubscriber extends MessageListenerAdapter {
     return sessions.get(getPlaceId(message));
   }
 
-  private void sendMessage(ChatReceiveMessage chatReceiveMessage, Set<WebSocketSession> targetSessionSet)
+  private void sendToSessions(ChatReceiveMessage chatReceiveMessage, Set<WebSocketSession> targetSessionSet)
       throws IOException {
     final TextMessage textMessage = new TextMessage(objectMapper.writeValueAsString(chatReceiveMessage));
 
