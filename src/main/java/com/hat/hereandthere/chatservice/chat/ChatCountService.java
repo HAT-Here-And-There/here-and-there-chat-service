b2410a1c68ecc1deaf9d5de5ca01dd61fa2ede0a
10000001a <pgdn06288888@gmail.com>
2024-07-31 23:02:51 +0900

    feat: handle when tour service has trouble

--- src/main/java/com/hat/hereandthere/chatservice/chat/ChatCountService.java
+++ src/main/java/com/hat/hereandthere/chatservice/chat/ChatCountService.java
@@ -1,13 +1,16 @@
 package com.hat.hereandthere.chatservice.chat;
 
 import com.hat.hereandthere.chatservice.chat.dto.GetPlaceMetaDto;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.core.env.Environment;
 import org.springframework.data.redis.core.RedisTemplate;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.client.RestClientException;
 import org.springframework.web.client.RestTemplate;
 
 
+@Slf4j
 @Service
 public class ChatCountService {
     final private RedisTemplate<String, Long> redisTemplate;
@@ -44,9 +47,14 @@ public class ChatCountService {
     }
 
     private GetPlaceMetaDto getPlaceMetadataFromExternal(Long placeId) {
-        String placeMetadataUrl = String.format("%s/places/meta/%d", env.getProperty("tour-service.url"), placeId);
+        try {
+            String placeMetadataUrl = String.format("%s/places/meta/%d", env.getProperty("tour-service.url"), placeId);
 
-        return restTemplate.getForObject(placeMetadataUrl, GetPlaceMetaDto.class);
+            return restTemplate.getForObject(placeMetadataUrl, GetPlaceMetaDto.class);
+        } catch (Exception e) {
+            throw new RestClientException("Failed to get place metadata from tour service");
+        }
+
     }
 
     private void cachePlaceMetadata(Long placeId, Long majorRegionId, Long sigunguId) {
@@ -59,13 +67,18 @@ public class ChatCountService {
     // add chat count
     @Transactional
     public void increaseChatCount(Long placeId) {
-        final GetPlaceMetaDto getPlaceMetaDto = getPlaceMetadataFromRedis(placeId);
+        try {
+            final GetPlaceMetaDto getPlaceMetaDto = getPlaceMetadataFromRedis(placeId);
 
-        redisTemplate.opsForZSet().incrementScore("place_chat_count", placeId, 1);
-        redisTemplate.opsForZSet()
-                .incrementScore(String.format("major_region:%d:chat_count", getPlaceMetaDto.majorRegionId()), placeId, 1);
-        redisTemplate.opsForZSet()
-                .incrementScore(String.format("sigungu:%d:chat_count", getPlaceMetaDto.sigunguId()), placeId, 1);
+            redisTemplate.opsForZSet().incrementScore("place_chat_count", placeId, 1);
+            redisTemplate.opsForZSet()
+                    .incrementScore(String.format("major_region:%d:chat_count", getPlaceMetaDto.majorRegionId()), placeId, 1);
+            redisTemplate.opsForZSet()
+                    .incrementScore(String.format("sigungu:%d:chat_count", getPlaceMetaDto.sigunguId()), placeId, 1);
+        } catch (RestClientException e) {
+            // tour-service와 연동 필요
+            log.error("increase chat count failed: {}", e.getMessage());
+        }
     }
 
 }
